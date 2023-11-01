20e51b5897b08b3237c99d611b1c24d787bf1cb0
alex <noirdragon@protonmail.com>
2023-10-31 23:10:37 -0400

    added main menu and changed wombat image

--- test.java
+++ test.java
@@ -15,18 +15,20 @@ public class test extends Actor
     public void act()
     {
         String key = Greenfoot.getKey();
-        int tileMovement = 5;
-        if("up".equals(key)){
+        int tileMovement = 50;
+        if("up".equals(key) || ("w".equals(key))){
             setLocation(getX(),getY()-tileMovement);
         }
-        if("down".equals(key)){
+        if("down".equals(key) || ("s".equals(key))){
             setLocation(getX(),getY()+tileMovement);
         }
-        if("right".equals(key)){
+        if("right".equals(key) || ("d".equals(key))){
             setLocation(getX()+tileMovement,getY());
+            setImage("wombat_right.png");
         }
-        if("left".equals(key)){
+        if("left".equals(key) || ("a".equals(key))){
             setLocation(getX()-tileMovement,getY());
+            setImage("wombat.png");
         }
     }
 }
