e59a9c82a254e30da7ddcd9ffec60c537c1260b4
Cy-EF0510 <6243515@edu.vaniercollege.qc.ca>
2023-11-16 18:44:50 -0500

    Settings world
    sttings world for the input and stuff

---
+++ SettingWorld.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class SettingWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class SettingWorld extends World
+{
+
+    /**
+     * Constructor for objects of class SettingWorld.
+     * 
+     */
+    public SettingWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
