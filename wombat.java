676cc92061f963c223b487b7dc0cc8a1c70536ac
Renz Ian Negrana <6225754@edu.vaniercollege.qc.ca>
2023-11-30 11:57:04 -0500

    turning codes in act to methods

--- wombat.java
+++ wombat.java
@@ -6,58 +6,10 @@ public class wombat extends Actor
     public GreenfootImage img = new GreenfootImage("wombat.png");
     public void act()
     {
-        boolean invincible = false;
+        onLog();
+        carGameOver();
         movementWombat();
-        Actor oak = getOneIntersectingObject(oakLog.class);
-        Actor birch = getOneIntersectingObject(birchLog.class);
-        Actor river = getOneIntersectingObject(river.class);
-        Actor placeholder = getOneIntersectingObject(placeholder.class);
-        Actor blueCar = getOneIntersectingObject(blueCar.class);
-        Actor portal = getOneIntersectingObject(grassPortal.class);
-        if (river != null && placeholder != null) {
-            invincible = true;
-            move(MyWorld.direction);
-            if (placeholder != null && birch != null)
-            {
-                setLocation(birch.getX(), birch.getY()-25);
-            }
-            else {
-                setLocation(oak.getX(), oak.getY()-25);
-            }
-        }
-        if(portal != null)
-        {
-            //invincible = true;
-            int timer = 0;
-            int width = img.getWidth();
-            int height = img.getHeight();
-            if (width == 2 || height == 2) {
-                Greenfoot.setWorld(new frostLevel());
-            }
-            if ((width >= 2 || height >= 2) && timer % 60 == 0 ) {
-                setLocation(portal.getX(), portal.getY());
-                turn(4);
-                img.scale(width/2, height/2);
-                setImage(img);
-            }
-            timer++;
-            //getWorld().removeObject(this);
-            //if (timer == 45) {
-            //    setImage("wombatHalf.png");
-            //} else if (timer == 90) {
-            //    setImage("wombatHalfOfAHalf.png");
-            //} else if (timer == 135) {
-            //    setImage("wombatSmol.png");
-            //} else if (timer == 180) {
-            //    setImage("wombatSuperSmol.png");
-            //}
-        }
-        if (river != null && invincible != true) {
-            Greenfoot.setWorld(new GameOverWorld());
-        }
-        if (blueCar != null) {
-            Greenfoot.setWorld(new GameOverWorld());
-        }
+        enterFrost();
     }
     public void movementWombat()
     {
@@ -99,5 +51,55 @@ public class wombat extends Actor
     public void moveScrollingWombat()
     {
         setLocation(getX(),getY()+1);
+    }
+    public void enterFrost()
+    {
+        Actor portal = getOneIntersectingObject(grassPortal.class);
+        if(portal != null)
+        {
+            //invincible = true;
+            int timer = 0;
+            int width = img.getWidth();
+            int height = img.getHeight();
+            if (width == 2 || height == 2) {
+                Greenfoot.setWorld(new frostLevel());
+            }
+            if ((width >= 2 || height >= 2) && timer % 60 == 0 ) {
+                setLocation(portal.getX(), portal.getY());
+                turn(4);
+                img.scale(width/2, height/2);
+                setImage(img);
+            }
+            timer++;
+        }
+    }
+    public void onLog()
+    {
+        boolean invincible = false;
+        Actor oak = getOneIntersectingObject(oakLog.class);
+        Actor birch = getOneIntersectingObject(birchLog.class);
+        Actor river = getOneIntersectingObject(river.class);
+        Actor placeholder = getOneIntersectingObject(placeholder.class);
+        if (river != null && placeholder != null) {
+            invincible= true;
+            move(MyWorld.direction);
+            if (placeholder != null && birch != null)
+            {
+                setLocation(birch.getX(), birch.getY()-25);
+            }
+            else {
+                setLocation(oak.getX(), oak.getY()-25);
+            }
+        }
+        if (river != null && invincible != true) {
+            Greenfoot.setWorld(new GameOverWorld());
+        }
+    }
+    public void carGameOver()
+    {
+        Actor blueCar = getOneIntersectingObject(blueCar.class);
+        if (blueCar != null) {
+            Greenfoot.setWorld(new GameOverWorld());
+        }
     }
 }
